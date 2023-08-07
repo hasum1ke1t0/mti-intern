acafd30d62952661484cafaba4529b39af1d015e
BlackNihonkai <c4557256@gmail.com>
2023-08-07 09:09:14 +0000

    nakagawa recipe get

--- lambda/team3-recipe-get.js
+++ lambda/team3-recipe-get.js
@@ -1,10 +1,13 @@
-const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBClient,
+  QueryCommand,
+  ScanCommand,
+} = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 const client = new DynamoDBClient({ region: "ap-northeast-1" });
-const TableName = "User";
+const TableName = "Recipe";
 
 exports.handler = async (event, context) => {
-  //レスポンスの雛形
   const response = {
     statusCode: 200,
     headers: {
@@ -12,28 +15,81 @@ exports.handler = async (event, context) => {
     },
     body: JSON.stringify({ message: "" }),
   };
+  /*
+  // 今回は簡易的な実装だが、一般的にはAuthorizationHeaderの値は、Authorization: <type> <credentials>のような形式になります。
+  // https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Authorization#%E6%A7%8B%E6%96%87
+  if (event.headers.authorization !== "mtiToken") {
+    response.statusCode = 401;
+    response.body = JSON.stringify({
+      message: "認証されていません。HeaderにTokenを指定してください",
+    });
 
-  const userId = event.queryStringParameters.userId; //見たいユーザのuserId
+    return response;
+  }
+  */
 
-  //TODO: 取得対象のテーブル名と検索に使うキーをparamに宣言
-  const param = {TableName,
-    Key:marshall({
-      userId
-    }),
-  };
-
-  // 指定したアイテムを取得するコマンドを用意
-  const command = new GetItemCommand(param);
-
+  const param = {
+    TableName,
+    Limit: 100,
+  }
+  
+  const scanCommand = new ScanCommand(param);
   try {
-    //client.send()の実行でDBからデータを取得
-    const user = (await client.send(command)).Item;
-
-    //TODO: 条件に該当するデータがあればパスワードを隠蔽をする処理を記述
-    delete user?.passeord;
-
-    //TODO: レスポンスボディに取得したUserの情報を設定する
-    response.body=JSON.stringify(unmarshall(user));
+    const articles = (await client.send(scanCommand)).Items;
+    console.log(1)
+  
+    if (articles.length == 0) {
+      response.body = JSON.stringify({ articles: [] });
+      console.log(2)
+    } else {
+      const unmarshalledArticles = articles.map((item) => unmarshall(item));
+      console.log(3)
+      //unmarshalledArticles.sort((a, b) => b.timestamp - a.timestamp);
+      const qs = event.queryStringParameters;
+      
+      console.log(4)
+      console.log(qs)
+      console.log(qs?.kcal)
+      
+      const hasKcal = qs?.kcal;
+      const hasAge = qs?.age;
+      let newUnmsKcal = [];
+      let newUnmsAge = [];
+      
+      console.log(5)
+      
+      // kcalでしぼりこみ
+      if (!hasKcal) {
+        //response.body = JSON.stringify({ articles: unmarshalledArticles });
+        newUnmsKcal = unmarshalledArticles;
+        console.log(6)
+      } else{
+        console.log(7)
+        for (let i = 0; i < unmarshalledArticles.length; i++) {
+          console.log(i)
+          if (unmarshalledArticles[i].kcal == hasKcal){
+            newUnmsKcal.push(unmarshalledArticles[i]);
+          }
+        }
+      }
+      // Ageでしぼりこみ
+      if (!hasAge) {
+        newUnmsAge = newUnmsKcal;
+        //response.body = JSON.stringify({ articles: newUnmsAge });
+      } else {
+        for (let i = 0; i < newUnmsKcal.length; i++) {
+          console.log(i)
+          if (newUnmsKcal[i].age == hasAge){
+            newUnmsAge.push(newUnmsKcal[i]);
+          }
+        }
+        //response.body = JSON.stringify({ articles: newUnmsAge });
+      }
+      // ランダムで選ぶ
+      const randMax = newUnmsAge.length;
+      var rand = Math.floor( Math.random() * randMax );
+      response.body = JSON.stringify({ articles: newUnmsAge[rand] });
+    }
   } catch (e) {
     response.statusCode = 500;
     response.body = JSON.stringify({
