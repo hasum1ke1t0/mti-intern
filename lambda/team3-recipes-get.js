e2656327e2f5e71c6bab399212d19f6c41739637
BlackNihonkai <c4557256@gmail.com>
2023-08-08 06:23:05 +0000

    nakagawa recipes get fix

--- lambda/team3-recipes-get.js
+++ lambda/team3-recipes-get.js
@@ -27,45 +27,111 @@ exports.handler = async (event, context) => {
     return response;
   }
   */
+  
+  function extractObjectsMatchingTarget(objectArray, targetIndices) {
+    console.log(7.1)
+    const matchingObjects = objectArray.filter(obj => {
+      console.log(7.2)
+      console.log(targetIndices)
+      const nestedArray = obj.material;
+      return targetIndices.every(index => {
+        if (nestedArray[index] === true) {
+          return true;
+        }
+      });
+    });
+    return matchingObjects;
+  }
+
 
   const param = {
     TableName,
-    Limit: 100,
+    //Limit: 100,
   }
   
   const scanCommand = new ScanCommand(param);
   try {
     const articles = (await client.send(scanCommand)).Items;
-    console.log(1)
+    //console.log(1)
   
     if (articles.length == 0) {
-      response.body = JSON.stringify({ articles: [] });
+      //response.body = JSON.stringify({ articles: [] });
       response.body = JSON.stringify({ recipes: [] });
-      console.log(2)
+      //console.log(2)
     } else {
       const unmarshalledArticles = articles.map((item) => unmarshall(item));
-      console.log(3)
+      //console.log(3)
       //unmarshalledArticles.sort((a, b) => b.timestamp - a.timestamp);
       const qs = event.queryStringParameters;
-      console.log(4)
-      console.log(qs)
-      console.log(qs?.material)
+      //console.log(4)
+      //console.log(qs)
+      //console.log(qs?.material)
       const hasValidQs = qs?.material;
       let newUnmsArticles = [];
-      console.log(5)
+      //console.log(5)
+      /* 動作確認済み始まり */
+      /*
       if (!hasValidQs) {
         //response.body = JSON.stringify({ articles: unmarshalledArticles });
         response.body = JSON.stringify({ recipes: unmarshalledArticles });
-        console.log(6)
+        //console.log(6)
       } else{
-        console.log(7)
+        //console.log(7)
         for (let i = 0; i < unmarshalledArticles.length; i++) {
-          console.log(i)
+          //console.log(i)
           if (unmarshalledArticles[i].material == hasValidQs){
             newUnmsArticles.push(unmarshalledArticles[i]);
           }
         }
         //response.body = JSON.stringify({ articles: newUnmsArticles });
+        response.body = JSON.stringify({ recipes: newUnmsArticles });
+      }
+      //*/
+      /* 動作確認済みおわり */
+      console.log(1)
+      if(!hasValidQs) {
+        console.log(2)
+        response.body = JSON.stringify({ recipes: unmarshalledArticles })
+      } else {
+        console.log(3)
+        const arr = hasValidQs.split(',');
+        let target = [];
+        console.log(3.5)
+        for( let g = 0; g < arr.length; g++) {
+          console.log(3.6)
+          if (arr[g] == 'true') {
+            target.push(g);
+            console.log(3.7)
+          }
+          else{
+            console.log(3.8)
+          }
+        }
+        console.log(4)
+        console.log(target)
+        /*for (let i = 0; i < unmarshalledArticles.length; i++) {
+          const uma = unmarshalledArticles[i].material;
+          console.log('5+'+i)
+          console.log('uma:'+uma)
+          console.log('uma[0]:'+uma[0])
+          // hasValidQsのうち、Trueのものが
+          for( let j = 0; j < target.length; j++) {
+            console.log('6+'+j)
+            const k = target[j]
+            //console.log('target['+j+']:'+k)
+            //console.log('uma[k]:'+uma[k])
+            if (uma[k] == true){
+              newUnmsArticles.push(uma[k]);
+              console.log(7)
+            }
+          }
+        }//*/
+        ///*
+        console.log(7)
+        const arrays = unmarshalledArticles;
+        const newUnmsArticles = extractObjectsMatchingTarget(arrays, target);
+        //*/
+        console.log(8)
         response.body = JSON.stringify({ recipes: newUnmsArticles });
       }
     }
